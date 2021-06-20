1e9c5f9043a605c822017cffbdd8dc4a59b80ac1
Giulia <giuliafiennaco@gmail.com>
2021-06-20 01:15:10 -0300

    Definições básicas da classe Alvo

---
+++ Codigo/alvo.js
@@ -0,0 +1,15 @@
+class Alvo{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    
+	draw(){
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, 30,	0, Math.PI*2);
+		ctx.fillStyle = 'red';
+		ctx.fill();
+	}
+}
+
+
