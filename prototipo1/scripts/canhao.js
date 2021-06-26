0b71d04441025b3fcc631fe01c6056b217edf018
Giulia <giuliafiennaco@gmail.com>
2021-06-26 16:45:26 -0300

    Inclusão de getAngul e getPosicao

--- prototipo1/scripts/canhao.js
+++ prototipo1/scripts/canhao.js
@@ -1,13 +1,22 @@
 var canhao_e_roda = new Array(1);
-var aux_angulo = 0.4375;
+var x = 30;
+var y;
+var canvasc;
+var contextc;
+var t = 0;
 class Canhao {
-  constructor(id) {
-    this.angulo = 0;
+  constructor(angulo, id) {
+    this.angulo = angulo;
     this.id = id;
     this.posicao = new Posicao(0, 0);
     this._init();
   }
-
+  getAngulo(){
+    return this.angulo;
+  }
+  getPosicao(){
+    return this.posicao;
+  }
   _init() {
     for (var i = 0; i < canhao_e_roda.length; i++) {
       canhao_e_roda[i] = new Array(2)
@@ -19,11 +28,15 @@ class Canhao {
       this.posicao.posY = canvas.height - 159;
     }
   }
+//Aqui só desenha a roda
   draw(context) {
     context.drawImage(canhao_e_roda[this.id][1], this.posicao.posX + 15, this.posicao.posY + 38, 55, 55);
   }
 
+//Aqui desenha o canhão no segundo canvas
   draw_(context, altura) {
+    // context.drawImage(canhao_e_roda[this.id][0], this.posicao.posX , this.posicao.posY, 112, 80);
+    // context.drawImage(canhao_e_roda[this.id][0], this.posicao.posX , this.posicao.posY , 119, 80);
     context.drawImage(canhao_e_roda[this.id][0], -44, -65, 116, 80);
   }
 
@@ -37,8 +50,8 @@ class Canhao {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     ctx.translate(x, y);
-    ctx.rotate(aux_angulo);
-    //  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    ctx.rotate(t);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
     this.draw_(ctx);
     ctx.restore();
     console.log("TEste");
@@ -54,29 +67,29 @@ class Canhao {
     // var auxc = this.transform - 19;
     var tecla = event.keyCode;
     if (tecla == 37) {
-      if (this.angulo < 90) {
-        // aux_angulo -= 0.03;
-        aux_angulo -= 0.0175;
+      if(t<=90){
+        t+=0.1;
       }
       // this.angulo = (this.angulo * Math.PI) / 180;
-      // console.log(t);
+      console.log(t);
     } else if (tecla == 38 && (pay >= -89)) {
       window.alert();
-      // this.angulo--;
+      this.angulo--;
     } else if (tecla == 39) {
-      if (this.angulo > 0) {
-        // aux_angulo += 0.03;
-        aux_angulo += 0.0175;
+      if(t>=0){
+        t-=0.1;
       }
     } else if (tecla == 40 && (pay <= -25)) {
       // this.angulo++;
-
+      
     } else if (tecla == 13) {
       py = (pay * (-1) * Math.PI) / 180;
       vx = vo * Math.cos(py);
       vy = vo * Math.sin(py);
     }
-    this.angulo = Math.floor(((180 * (-aux_angulo + 0.4375)) / Math.PI));
-    console.log("Angulo= " + this.angulo + "t= " + aux_angulo);
+    this.angulo= (t*Math.PI)/180;
+
   }
+
+
 }
