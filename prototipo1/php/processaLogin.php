6a69d0443f0ee4e08cfcd0d343c8fc8221dbb4df
Giulia <giulia.iennaco@estudante.ufjf.br>
2021-08-12 19:12:04 -0300

    Autenticação de Login

--- prototipo1/php/processaLogin.php
+++ prototipo1/php/processaLogin.php
@@ -6,78 +6,52 @@ require_once ('connect.php');
 // print_r($_POST);
 // echo '</pre>';
 
-// //require('connect.php');
+session_start();
+if(!empty($_POST)) {
+    try{
+        $sql = "SELECT USER_EMAIL, USER_NASCIMENTO, USER_NAME, USER_PONT_TOTAL, USER_ULTM_FASE FROM TB_USER WHERE USER_EMAIL = :email and USER_SENHA = :senha";
 
-// die();
+        $stmt = $conn->prepare($sql);
 
-try{
-    $sql = "SELECT USER_EMAIL, USER_NASCIMENTO, USER_NAME, USER_PONT_TOTAL, USER_ULTM_FASE FROM TB_USER WHERE USER_EMAIL = :email and USER_SENHA = :senha";
-
-    $stmt = $conn->prepare($sql);
-
-    $dados = array(
-        ':email' => $_POST['email'],
-        ':senha' => md5($_POST['senha'])
-    );
-    
-    $stmt->execute($dados);
-
-    $result = $stmt->fetchAll();
-    if($stmt->rowCount() == 1){
-        $result = $result[0];
-
+        $dados = array(
+            ':email' => $_POST['email'],
+            ':senha' => md5($_POST['senha'])
+        );
         
-    } 
-    else {
+        $stmt->execute($dados);
 
+        $result = $stmt->fetchAll();
+        if($stmt->rowCount() == 1){
+            $result = $result[0];
+
+            $_SESSION['nome'] = $result['user_name'];
+            $_SESSION['email'] = $result['user_email'];
+            $_SESSION['nascimento'] = $result['user_nascimento'];
+            $_SESSION['pontTotal'] = $result['user_pont_total'];        
+            $_SESSION['ultimaFase'] = $result['user_ultm_fase'];
+
+
+            header("Location: ../index.html");
+
+        } 
+        else {
+            session_destroy();
+            header("Location: ../login.html?msgErro=Usuário ou senha incorretos.");
+        }
+        
+
+        echo '<pre>';
+        print_r($result);
+        echo '</pre>';
+
+
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
     }
-    
-
-    echo '<pre>';
-    print_r($result);
-    echo '</pre>';
-
-
-    // if($stmt->execute($dados)){
-    //     header("Location: ../index.html");
-    // }
-
-} catch (PDOException $e) {
-    echo $e->getMessage();
 }
-
-// try{
-//     // $nome = $_POST['nome'];
-//     // $email = $_POST['email'];
-//     // $senha = md5($_POST['senha']);
-//     // $dataNascimento = $_POST['dataNascimento'];
-
-//     echo "teste";
-
-//     $sql = "INSERT INTO tb_user (user_name, user_email, user_nascimento, user_senha) VALUES (:nome, :email, :dataNascimento, :senha)";
-
-
-//     $stmst = $conn->prepare($sql);
-
-//     $dados = array(
-//         ':nome' => $_POST['nome'],
-//         ':email' => $_POST['email'],
-//         ':dataNascimento' => $_POST['dataNascimento'],
-//         ':senha' => md5($_POST['senha'])
-//     );
-    
-//     if($stmst->execute($dados)){
-//         header("Location: ../index.html");
-//     }
-    
-//     // if(!$query) {
-//     //     echo $sql->errno;
-//     // }
-//     // else {
-//     //     header('Location: ../cadastro.php');
-//     // }    
-// } catch (PDOException $e) {
-//     echo $e->getMessage();
-// }
+else {
+    header("Location: ../login.html?msgErro=Sem Permissão.");
+}
 
  ?>
