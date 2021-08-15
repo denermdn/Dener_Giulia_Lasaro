00e8146911a80832805fb9d5af0b49601f782800
Giulia <giulia.iennaco@estudante.ufjf.br>
2021-08-15 00:16:54 -0300

    Limpeza de código

--- prototipo1/php/connect.php
+++ prototipo1/php/connect.php
@@ -2,7 +2,6 @@
 try{ 
 
     $conn = new PDO('pgsql:host=localhost;port=5432;dbname=tbpt', 'tbpt_execute','engsoft2021', [PDO::ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION]);
-    echo "database conectado";
 
     
     }catch (PDOException $e){
